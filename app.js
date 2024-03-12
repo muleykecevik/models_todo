ab3c524b788dff11a51d6bd1a870b4944c6787c0
muleykecevik <muleyke.cevik@gmail.com>
2024-03-12 21:03:47 +0300

    12/3/24

--- app.js
+++ app.js
@@ -83,63 +83,82 @@ sequelize.authenticate() //database e baglanma methodu
 const router = express.Router()
 
 // // LIST TODOS:
-router.get('/', async (req, res) => {
+// router.get('/', async (req, res) => {
 
-    // const data = await Todo.findAll()
-    const data = await Todo.findAndCountAll() //tum kayitlarin sayisini verir
+//     // const data = await Todo.findAll()
+//     const data = await Todo.findAndCountAll() //tum kayitlarin sayisini verir
 
-    res.status(200).send({
-        error: false,
-        result: data
-    })
-})
+//     res.status(200).send({
+//         error: false,
+//         result: data
+//     })
+// })
 
 //? CRUD Processes:
 
 // CREATE TODO:
-router.post('/', async (req, res) => {
+// router.post('/', async (req, res) => {
 
-    const receivedData = req.body
+//     const receivedData = req.body
 
-    // const data = await Todo.create({
-    //     title: receivedData.title,
-    //     description: receivedData.description,
-    //     priority: receivedData.priority,
-    //     isDone: receivedData.isDone,
-    //     // newKey: 'newValue' // Modelde tanımlanmadığı için bir işe yaramayacaktır.
-    // })
+// const data = await Todo.create({
+//     title: receivedData.title,
+//     description: receivedData.description,
+//     priority: receivedData.priority,
+//     isDone: receivedData.isDone,
+//     // newKey: 'newValue' // Modelde tanımlanmadığı için bir işe yaramayacaktır.
+// })
 
-    const data = await Todo.create(req.body)
+//     const data = await Todo.create(req.body)
 
-    res.status(201).send({
-        error: false,
-        result: data.dataValues
-    })
-})
+//     res.status(201).send({
+//         error: false,
+//         result: data.dataValues
+//     })
+// })
 
 // READ TODO:
-router.get('/:id', async (req, res) => {
+// router.get('/:id', async (req, res) => {
 
-    //const data = await Todo.findOne({ where: { id: req.params.id } })
-    const data = await Todo.findByPk(req.params.id) //await ayazdigimiz icin yukari async yaziyoruz
+//     //const data = await Todo.findOne({ where: { id: req.params.id } })
+//     const data = await Todo.findByPk(req.params.id) //await ayazdigimiz icin yukari async yaziyoruz
 
-    res.status(200).send({
-        error: false,
-        result: data
-    })
+//     res.status(200).send({
+//         error: false,
+//         result: data
+//     })
 
-})
+// })
 
 //Uptade todo
 router.put('/:id', async (req, res) => {
 
-    //const data = await Todo.update({   } })
-    const data = await Todo.update(req.params.id)
+    //const data = await Todo.update({....newdata}, {where} })
+    const data = await Todo.update(req.body, { where: { id: req.params.id } })
 
-    res.status(200).send({
+    res.status(202).send({ //guncelleme status kodu
         error: false,
-        result: data
+        message: "updated",
+        body: req.body,
+        result: data,  //kac kayit guncelledim onu gosterir
+        new: await Todo.findByPk(req.params.id) //gncelledigim datayi gosterir
     })
+
+})
+
+//Delete todo
+
+router.delete('/:id', async (req, res) => {
+
+    //const data = await Todo.destroy({where} })
+    const data = await Todo.destroy({ where: { id: req.params.id } })
+
+    res.status(204).send({ //silme status kodu
+        error: false,
+        message: "deleted",
+        result: data,  //kac kayit sildim onu gosterir
+
+    }) // 204 no content icerik vermeyebilri. 
 
 })
 
